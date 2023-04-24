ff32431ad07b55bf619863e90e69a55e6201a0e8
Rémi Salles <remisalles.12@gmail.com>
2023-04-24 03:00:26 +0200

    responsive sauf navbar

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Sick caroussel
+$(document).ready(function(){
+    $('.image-logo').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+
+      ]
+    });
+  });
